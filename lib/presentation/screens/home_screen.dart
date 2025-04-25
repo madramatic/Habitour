c0afbe533491e84d35767c7b6482df9598797316
Hammad <hammadasif0027@gmail.com>
2025-04-25 12:06:04 +0500

    feat(home_screen): update layout to use Stack for better positioning
    - Replaced `Padding` with `Stack` to allow more flexible positioning of elements.
    - Moved the `SecondaryButton` to a `Positioned` widget for precise placement at the bottom of the screen.
    - Adjusted padding and spacing for improved layout consistency.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:habitour/presentation/widgets/custom_button_widget.dart';
 import 'package:intl/intl.dart';
 import 'package:habitour/presentation/widgets/horizontal_calendar_widget.dart';
+import '../widgets/custom_button_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,39 +17,44 @@ class _HomeScreenState extends State<HomeScreen> {
         DateFormat('d MMMM yyyy').format(DateTime(2025, 4, 23));
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
               children: [
-                Text(
-                  todayDate,
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32,
-                      ),
+                const SizedBox(height: 60),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      todayDate,
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32,
+                          ),
+                    ),
+                    Image.asset(
+                      'assets/images/checklist_left.png',
+                      height: 100.0,
+                    ),
+                  ],
                 ),
-                Image.asset(
-                  'assets/images/checklist_left.png',
-                  height: 100.0,
-                ),
+                const HorizontalCalendarWidget(),
               ],
             ),
-            const SizedBox(height: 20),
-            const HorizontalCalendarWidget(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: SecondaryButton(
-          label: "New habit, who this?",
-          onPressed: () {},
-          icon: Icons.my_library_add_rounded,
-        ),
+          ),
+          Positioned(
+            bottom: 40.0,
+            left: 40.0,
+            right: 40.0,
+            child: SecondaryButton(
+              label: "New habit, who this?",
+              onPressed: () {},
+              icon: Icons.my_library_add_rounded,
+            ),
+          ),
+        ],
       ),
     );
   }
