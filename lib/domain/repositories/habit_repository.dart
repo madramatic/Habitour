41bb086776689290e401992f86b6560564e81b2f
Hammad <hammadasif0027@gmail.com>
2025-05-12 13:36:26 +0500

    feat(domain): add updateHabit method to HabitRepository

--- lib/domain/repositories/habit_repository.dart
+++ lib/domain/repositories/habit_repository.dart
@@ -2,6 +2,7 @@ import '../entities/habit.dart';
 
 abstract class HabitRepository {
   Future<void> addHabit(Habit habit);
+  Future<void> updateHabit(Habit habit);
   Future<void> deleteHabit(String id);
   Future<List<Habit>> getAllHabits();
 }
