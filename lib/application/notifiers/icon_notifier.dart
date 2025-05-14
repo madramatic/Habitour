4d031751555cea4393fef812eb85d9f0a7a4525b
Hammad <hammadasif0027@gmail.com>
2025-05-14 15:50:41 +0500

    feat(icon_notifier): add IconNotifier for managing the state of selected icon

---
+++ lib/application/notifiers/icon_notifier.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:habitour/core/models/icon_enum.dart';
+
+class IconNotifier extends StateNotifier<HabitIcon?> {
+  IconNotifier() : super(null);
+
+  void selectIcon(HabitIcon? icon) {
+    state = icon;
+  }
+}
+
+final iconNotifierProvider = StateNotifierProvider<IconNotifier, HabitIcon?>(
+  (ref) => IconNotifier(),
+);
